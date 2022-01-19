c7d7324215dbac20011b4a3a2171e619daf3a9c3
Mikolaj Sujka <mikolaj.sujka@qodeca.com>
2022-01-19 12:49:00 +0100

    Adding pages, styiling etc.

--- YourMarket/src/app/app-routing.module.ts
+++ YourMarket/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // components 
+import { HomePageComponent } from './home-page/home-page.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: SignInComponent, data: { title: 'sign-in', depth: 1 }},
-  { path: 'register', component: SignUpComponent, data: { title: 'sign-up', depth: 2}}
+  { path: 'register', component: SignUpComponent, data: { title: 'sign-up', depth: 2}},
+  { path: 'home-page', component: HomePageComponent}
 ];
 
 @NgModule({
--- YourMarket/src/app/app.module.ts
+++ YourMarket/src/app/app.module.ts
@@ -2,16 +2,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 // components
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { ContactViewComponent } from './home-page/contact-view/contact-view.component';
+
+// 3rd
 
 @NgModule({
-  declarations: [AppComponent, SignInComponent, SignUpComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    NavigationComponent,
+    HomePageComponent,
+    FooterComponent,
+    SearchPageComponent,
+    ContactViewComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
