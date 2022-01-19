c790430cdde42bba428c285fd52189014563f4fd
Mikolaj Sujka <mikolaj.sujka@qodeca.com>
2022-01-19 22:09:15 +0100

    Improvements etc.

--- YourMarket/src/app/app-routing.module.ts
+++ YourMarket/src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 // components 
 import { HomePageComponent } from './home-page/home-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: '', component: SignInComponent, data: { title: 'sign-in', depth: 1 }},
   { path: 'register', component: SignUpComponent, data: { title: 'sign-up', depth: 2}},
-  { path: 'home-page', component: HomePageComponent}
+  { path: 'home-page', component: HomePageComponent},
+  { path: 'search-page', component: SearchPageComponent}
 ];
 
 @NgModule({
