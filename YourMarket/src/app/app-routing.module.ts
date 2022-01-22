79b7b493df2ebffe7d3c91a13224aba2da1e1876
Mikolaj Sujka <mikolaj.sujka@qodeca.com>
2022-01-22 21:47:43 +0100

    Improving order-history page

--- YourMarket/src/app/app-routing.module.ts
+++ YourMarket/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MyBasketViewComponent } from './my-basket-view/my-basket-view.component';
 import { MyProfileViewComponent } from './my-profile-view/my-profile-view.component';
+import { OrderHistoryComponent } from './order-history/order-history.component';
 import { SearchPageComponent } from './search-page/search-page.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
@@ -15,7 +16,8 @@ const routes: Routes = [
   { path: 'home-page', component: HomePageComponent},
   { path: 'search-page', component: SearchPageComponent},
   { path: 'my-basket', component: MyBasketViewComponent},
-  { path: 'my-profile', component: MyProfileViewComponent}
+  { path: 'my-profile', component: MyProfileViewComponent},
+  { path: 'order-history', component: OrderHistoryComponent}
 ];
 
 @NgModule({
