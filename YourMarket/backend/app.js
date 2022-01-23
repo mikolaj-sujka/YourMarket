445b4fe9261430e78f8692fa4e738e35640d2ab8
Mikolaj Sujka <mikolaj.sujka@qodeca.com>
2022-01-23 16:28:30 +0100

    Adding backend files, packages, express.js etc.

---
+++ YourMarket/backend/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const authRoutes = require('./routes/auth')
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://admin123:Password123@cluster0.yiaj1.mongodb.net/yourMarketDb?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+// app.use("/api/auth", authRoutes);
+
+module.exports = app;
