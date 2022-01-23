2d5413c80a43636b1835362b67d15410d0012c21
Mikolaj Sujka <mikolaj.sujka@qodeca.com>
2022-01-23 13:29:24 +0100

    Improvement of validators

--- YourMarket/src/app/sign-up/sign-up.component.ts
+++ YourMarket/src/app/sign-up/sign-up.component.ts
@@ -1,31 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
   signUpForm: FormGroup;
+  checkedCheckbox: boolean = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.initializeForm();
-    console.log(this.signUpForm)
+    console.log(this.signUpForm);
   }
 
   initializeForm() {
     this.signUpForm = this.formBuilder.group({
-      email: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
       nip: ['', [Validators.required]],
       postalCode: ['', [Validators.required]],
       nameOfCompany: ['', [Validators.required]],
       city: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-      repeatPassword: ['', [Validators.required]]
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(12)]],
+      repeatPassword: ['', [Validators.required, this.matchValues('password')]],
     });
   }
 
+  onClickCheckbox(event: any) {
+    if (event.target.checked) {
+      this.checkedCheckbox = true;
+    }
+  }
+
+  matchValues(matchTo: string) : ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value 
+        ? null : {isMatching: true}
+    }
+  }
 }
