63797d61ce453f72eac6b55c3605ca8750a6b3e2
Mikolaj Sujka <mikolaj.sujka@qodeca.com>
2022-01-25 12:00:21 +0100

    Improving navigation, improving contact form

--- YourMarket/src/app/home-page/contact-view/contact-view.component.ts
+++ YourMarket/src/app/home-page/contact-view/contact-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-contact-view',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-view.component.scss']
 })
 export class ContactViewComponent implements OnInit {
+  contactForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private toastrService: ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.contactForm = this.formBuilder.group({
+      email: ['', 
+        [Validators.required, Validators.email]
+      ],
+      phoneNumber: ['',
+        [Validators.required, Validators.minLength(9)]
+      ],
+      nameOfCompany: ['',
+        [Validators.required]
+      ],
+      additionalInfo: ['']
+    })
+  }
+
+  onSend() {
+    this.toastrService.success("Successfully sent contact info!", "Contact info send", {
+      positionClass: 'toast-bottom-center'
+    })
+    this.contactForm.reset();
   }
 
 }
